8233db9c6196176cdbce68330a432240c2bb39f8
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-03-05 23:17:07 +0330

    Store Report

--- controllers/admin/reportController.js
+++ controllers/admin/reportController.js
@@ -5,7 +5,6 @@ const Product = require('models/product')
 const Payment = require('models/payment')
 const ticketsReport = require('serverModules/ticketsReport')
 
-
 module.exports = new class reportController extends controller {
     async generalTicketsReport(req, res, next) {
         try {
@@ -84,6 +83,42 @@ module.exports = new class reportController extends controller {
                 sellValues
            }
             res.render('admin/sellReport')
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    async generalStoreReport (req, res, next) {
+        try {
+
+            const userID = req.user.id
+            const adminDepartment = req.user.department
+            const userTickets = await Ticket.find({ $or: [{ user: userID }, { targetDepartment: adminDepartment }]})
+            const ticketNumber = ticketsReport(userTickets)
+            const recevedTicketsNumber = ticketNumber.recevedTicketsNumber
+            const sentTicketsNumber = ticketNumber.sentTicketsNumber
+            const allTicketsNumber = ticketNumber.allTicketsNumber
+
+            const countOfProduct = await Product.find()
+            let titles = []
+            let values = []
+            Object.values(countOfProduct).forEach(counts => {
+                titles.push(counts.title)
+                values.push(counts.count)
+            })
+
+            console.log(titles)
+            console.log(values)
+
+            res.locals = {
+                persianDate,
+                recevedTicketsNumber,
+                sentTicketsNumber,
+                allTicketsNumber,
+                // sellTitles,
+                // sellValues
+           }
+            res.render('admin/storeReport')
         } catch (err) {
             next(err)
         }
--- routes/admin/report.js
+++ routes/admin/report.js
@@ -4,5 +4,6 @@ const reportController = require('controllers/admin/reportController')
 
 router.get('/ticket', reportController.generalTicketsReport)
 router.get('/sell', reportController.generalSellReport)
+router.get('/store', reportController.generalStoreReport)
 
 module.exports = router
